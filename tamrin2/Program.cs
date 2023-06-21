6178c5585651662a6d7fc9f4686398d6e0193357
Mohamad alavi <Mohamad alavi@DESKTOP-9QCQADM>
2023-06-21 09:14:09 -0700

    Add project files.

---
+++ tamrin2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tamrin2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inter x");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"adad bad az mohasebe = {mohasebe(x)}");
+
+            static double mohasebe(int X)
+            {
+                int a = (int)(Math.Pow(X, 2) + (X * X) - 4);
+                return a;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
